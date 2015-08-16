04055c5ad5af277e609ac4b0c3295b9f844a264f
Nikhil Lopes <nikhillopes@gmail.com>
2015-08-16 19:39:15 -0400

    gm2 after vid built

--- app/build/generated/source/r/release/edu/buffalo/cse/cse486586/groupmessenger2/R.java
+++ app/build/generated/source/r/release/edu/buffalo/cse/cse486586/groupmessenger2/R.java
@@ -28,26 +28,26 @@ public final class R {
         public static final int activity_group_messenger=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int button_ptest=0x7f040001;
-        public static final int button_send=0x7f040002;
-        public static final int button_test1=0x7f040003;
-        public static final int button_test2=0x7f040004;
-        public static final int edittext_hint=0x7f040005;
-        public static final int menu_settings=0x7f040006;
+        public static final int app_name=0x7f050000;
+        public static final int button_ptest=0x7f050001;
+        public static final int button_send=0x7f050002;
+        public static final int button_test1=0x7f050003;
+        public static final int button_test2=0x7f050004;
+        public static final int edittext_hint=0x7f050005;
+        public static final int menu_settings=0x7f050006;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f040000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f040001;
     }
 }
